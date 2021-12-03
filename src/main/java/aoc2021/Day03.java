617c195e76b69e63ae4e4b1fbe278d6a10a54d64
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2021-12-03 10:19:35 +0300

    style: Remove unused dependencies

--- src/main/java/aoc2021/Day03.java
+++ src/main/java/aoc2021/Day03.java
@@ -2,9 +2,7 @@ package aoc2021;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Day03 {
     public static void main(String[] args) throws FileNotFoundException {
