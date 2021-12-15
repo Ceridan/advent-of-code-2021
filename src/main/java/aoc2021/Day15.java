fa08e9ded3f73687d51c303eb36ccc312916a17f
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2021-12-15 09:57:12 +0300

    feat: Day 15: working Dijkstra, not working solution

---
+++ src/main/java/aoc2021/Day15.java
@@ -0,0 +1,126 @@
+package aoc2021;
+
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Day15 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        List<String> data = Utils.readInputAsStringArray("day15.txt");
+
+        System.out.printf("Day 15, part 1: %d\n", part1(data));
+        System.out.printf("Day 15, part 2: %d\n", part2(data));
+    }
+
+    static int part1(List<String> data) {
+        int maxX = data.size();
+        int maxY = data.get(0).length();
+        List<Edge>[] edges = buildEdges(data);
+        return dijkstra(edges, 0, maxX * maxY - 1);
+    }
+
+    static int part2(List<String> data) {
+        return 0;
+    }
+
+    private static int dijkstra(List<Edge>[] edges, int startVertexId, int endVertexId) {
+        int n = edges.length;
+        int[] dist = new int[n];
+        int[] path = new int[n];
+        PriorityQueue<VertexRisk> pq = new PriorityQueue<>(n, Comparator.comparingInt(e -> e.risk));
+
+        for (int i = 0; i < n; i++) {
+            dist[i] = i == startVertexId ? 0 : Integer.MAX_VALUE;
+            path[i] = -1;
+            pq.add(new VertexRisk(i, dist[i]));
+        }
+
+        while (pq.size() > 0) {
+            VertexRisk vr = pq.poll();
+            int u = vr.vertex;
+
+            if (dist[u] == Integer.MAX_VALUE || dist[u] < vr.risk) {
+                continue;
+            }
+
+            for (Edge edge : edges[u]) {
+                int v = edge.target;
+                int risk = edge.risk;
+
+                if (dist[v] > dist[u] + risk) {
+                    dist[v] = dist[u] + risk;
+                    path[v] = u;
+                    pq.add(new VertexRisk(v, dist[v]));
+                }
+            }
+        }
+
+        printPath(path, startVertexId, endVertexId);
+
+        return dist[endVertexId];
+    }
+
+    private static void printPath(int[] path, int startVertexId, int endVertexId) {
+        System.out.println();
+
+        int currentVertexId = endVertexId;
+
+        while (currentVertexId != startVertexId) {
+            System.out.printf("%d <- ", currentVertexId);
+            currentVertexId = path[currentVertexId];
+        }
+
+        System.out.printf("%d", currentVertexId);
+
+        System.out.println();
+    }
+
+    private static List<Edge>[] buildEdges(List<String> data) {
+
+        int maxX = data.size();
+        int maxY = data.get(0).length();
+
+        List<Edge>[] edges = new List[maxX * maxY];
+
+        for (int y = 0; y < maxY; y++) {
+            for (int x = 0; x < maxX; x++) {
+                int vertexId = y * maxX + x;
+                edges[vertexId] = new ArrayList<>();
+
+                if (x < maxX - 1) {
+                    int cost = Integer.parseInt(data.get(y), x + 1, x + 2, 10);
+                    edges[vertexId].add(new Edge(vertexId, vertexId + 1, cost));
+                }
+
+                if (y < maxY - 1) {
+                    int cost = Integer.parseInt(data.get(y + 1), x, x + 1, 10);
+                    edges[vertexId].add(new Edge(vertexId, (y + 1) * maxX + x, cost));
+                }
+            }
+        }
+
+        return edges;
+    }
+
+    private static class VertexRisk {
+        private final int vertex;
+        private final int risk;
+
+        private VertexRisk(int vertex, int risk) {
+            this.vertex = vertex;
+            this.risk = risk;
+        }
+    }
+
+    private static class Edge {
+        private final int source;
+        private final int target;
+        private final int risk;
+
+        private Edge(int source, int target, int risk) {
+            this.source = source;
+            this.target = target;
+            this.risk = risk;
+        }
+    }
+}
---
+++ src/test/java/aoc2021/Day15Tests.java
@@ -0,0 +1,35 @@
+package aoc2021;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day15Tests {
+    private static final List<String> data = List.of(
+        "1163751742",
+        "1381373672",
+        "2136511328",
+        "3694931569",
+        "7463417111",
+        "1319128137",
+        "1359912421",
+        "3125421639",
+        "1293138521",
+        "2311944581");
+
+    @Test
+    void part1_example() {
+        int result = Day15.part1(data);
+
+        assertEquals(40, result);
+    }
+
+    @Test
+    void part2_example() {
+        int result = Day15.part2(data);
+
+        assertEquals(0, result);
+    }
+}
